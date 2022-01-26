adadef93996ad58a72e84e69996551fec0a562ba
ClotildeBRAYE <78138566+ClotildeBRAYE@users.noreply.github.com>
2022-01-26 11:36:31 +0100

    Update RandomForest.py

--- RandomForest.py
+++ RandomForest.py
@@ -7,15 +7,12 @@ Created on Tue Jan 25 17:29:42 2022
 
 
 import numpy as np
-from sklearn.datasets import make_blobs
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-data = pd.read_csv (r'C:/Users/Clo/Documents/MasterIA4OneHealth/Hackathon/data/preprocessed_data.csv')   #read the csv file (put 'r' before the path string to address any special characters in the path, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
-#print (data)
+data = pd.read_csv (r'C:/Users/Clo/Documents/MasterIA4OneHealth/Hackathon/data/preprocessed_data.csv')  
 
 X = pd.DataFrame(data, columns= ['gender','age','ever_married','work_type','Residence_type','avg_glucose_level','bmi','smoking_status'])
 X = StandardScaler().fit_transform(X)
@@ -28,16 +25,18 @@ X_train, X_test, Y_stroke_train, Y_stroke_test, Y_HD_train,Y_HD_test, Y_HT_train
 
   
 
-#RandomForestClassifier for stroke
+#RandomForest for stroke
 from sklearn.ensemble import RandomForestClassifier
 model_1_stroke = RandomForestClassifier(n_estimators=10, random_state=0)
 model_1_stroke.fit(X_train, np.ravel(Y_stroke_train)).predict_proba(X_test)
 print("Accuracy for stroke model is:",model_1_stroke.score(X_test,Y_stroke_test))
 
+#RandomForest for heart disease
 model_1_HD = RandomForestClassifier(n_estimators=10, random_state=0)
 model_1_HD.fit(X_train, np.ravel(Y_HD_train)).predict_proba(X_test)
 print("Accuracy for heart disease model is:",model_1_HD.score(X_test,Y_HD_test))
 
+#RandomForest for hypertension 
 model_1_HT = RandomForestClassifier(n_estimators=10, random_state=0)
 model_1_HT.fit(X_train, np.ravel(Y_HT_train)).predict_proba(X_test)
 print("Accuracy for hypertension model is:",model_1_HT.score(X_test,Y_HT_test))
